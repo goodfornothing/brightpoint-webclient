a5fb74967872ef35d2bcf7a108f3aa794b42eb2a
paul <paul@technotomy.com>
2012-05-19 13:34:53 +0100

    data read working

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,31 @@
-require(["jquery", "html5shiv"], function($) {
+require(["jquery", "html5shiv", "models/chromosome"], function($) {
     $(function() {
+    	var chromosome = new ChromosomeSection();
+    	var height = 400, width = 800, graph = $('#chromosomeGraph .data');
+
+    	graph.css({
+    		height: height,
+    		width: width
+    	});
+    	console.log(graph);
+
+    	var i, items = chromosome.getData().items, 
+    		start = chromosome.getData().section.x1,
+    		end = chromosome.getData().section.x2, domItems = [];
+    	for(i in chromosome.getData().items){
+    		var item = items[i];
+    		var point = $('div', {"class":"point"});
+    		var css = {
+    			left : item.start - start,
+	    		width : item.end - start,
+	    		top : (1 - item.y) * height,
+	    		height : 5,
+	    		position : 'relative'
+	    	};
+    		point.css(css);
+    		domItems.push(point);
+    	}
+    	console.log(domItems);
+    	$('#chromosomeGraph .data').append(domItems);
     });
 });
