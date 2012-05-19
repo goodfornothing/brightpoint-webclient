49bd607ee0d69442f6a4e78f94e1169aa4bb2650
paul <paul@technotomy.com>
2012-05-19 18:45:03 +0100

    live data connected

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,37 @@
 $(function() {
 	
 
-    var chromosomeSection = new ChromosomeSection(), chromosome = chromosomeSection.getData();
-    console.log(chromosome);
-    var axisx = [],
+    var chromosomeSection = new SectionAdapter();
+    $.ajax({
+    	url: 'http://brightpoint.herokuapp.com/api/v1/subjects.json',
+    	type: 'GET',
+    	dataType: 'json',
+    	success: function(data){
+    		console.log(data);
+    		chromosomeSection.setData(data[0]);
+    		plotGraph(chromosomeSection.getData());
+    	}
+    });
+    
+});
+
+function plotGraph(chromosome){
+	console.log(chromosome.data_points);
+	var axisx = [],
         axisy = [],
         maxY = 0, minY = 0,
-        maxX = chromosome.section.x2, minX = chromosome.section.x1;
-    $.each(chromosome.items, function(i, item){
+        maxX = 0, minX = 0;
+    $.each(chromosome.data_points, function(i, item){
+    	console.log(item);
 		if(item.y > maxY) maxY = item.y;
 		if(item.y < minY) minY = item.y;
+		var x = item.end + item.start / 2;
+		if(x > maxX) maxX = x;
+		if(x < minX) minX = x;
     });
     // Draw
-    var width = 1300,
-        height = 760,
+    var width = 2300,
+        height = 360,
         leftgutter = 100,
         bottomgutter = 30,
         topgutter = 30;
@@ -26,20 +44,22 @@ $(function() {
         r.text(25, topgutter + (y * (height - bottomgutter - topgutter) / 21), roundNumber(((y * deltaY) - maxY), 4 ) ).attr(txt);
     }
     for (var x = 0; x <= 10; x++ ) {
-        r.text( leftgutter + (x * width / 11), 750, (x * deltaX) + minX).attr(txt);
+        r.text( leftgutter + (x * width / 11), height - 10, Math.round((x * deltaX) + minX)).attr(txt);
     }
-    console.log(chromosome.items.length);
-    $.each(chromosome.items, function(i, item){
+    console.log(chromosome.data_points.length);
+    $.each(chromosome.data_points, function(i, item){
     	console.log(item);
     	console.log(' - - - - ');
-    	var x = leftgutter +((item.start - minX) / (maxX - minX) * width);
+    	var pointX = item.end + item.start / 2;
+    	var x = leftgutter +((pointX - minX) / (maxX - minX) * width);
     	console.log((maxY - minY));
     	console.log((maxY - item.y));
     	var y = topgutter + (maxY - item.y) / (maxY - minY) * height;
     	console.log(y);
     	r.circle(x, y, 2).attr({stroke: "none", fill: "#000", opacity: 1});;
     });
-});
+
+}
 
 function roundNumber(num, dec) {
 	var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
