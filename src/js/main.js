f97e96442bb04c1874fe96c70f1dd96d0ad7b77c
paul <paul@technotomy.com>
2012-05-19 19:10:09 +0100

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,6 @@ $(function() {
     	type: 'GET',
     	dataType: 'json',
     	success: function(data){
-    		console.log(data);
     		chromosomeSection.setData(data[0]);
     		plotGraph(chromosomeSection.getData());
     	}
@@ -16,13 +15,11 @@ $(function() {
 });
 
 function plotGraph(chromosome){
-	// console.log(chromosome.data_points);
 	var axisx = [],
         axisy = [],
         maxY = 0, minY = 0,
         maxX = 0, minX = 0;
     $.each(chromosome.data_points, function(i, item){
-    	console.log(item);
 		if(item.y > maxY) maxY = item.y;
 		if(item.y < minY) minY = item.y;
 		var x = item.end + item.start / 2;
@@ -38,7 +35,7 @@ function plotGraph(chromosome){
         contentWidth = width - leftgutter,
         contentHeight = height - topgutter - bottomgutter;
         r = Raphael("chart", width, height),
-        txt = {"font": 'Nokia Pure Text, Arial', stroke: "none", fill: "#fff"},
+        txt = {"font": 'Nokia Pure Text, Arial', stroke: "none", fill: "#f22"},
         color = $("#chart").css("color"),
         deltaX = (maxX - minX) / 10,
         deltaY = (maxY - minY) / 20;
@@ -48,7 +45,6 @@ function plotGraph(chromosome){
     for (var x = 0; x <= 10; x++ ) {
         r.text( leftgutter + (x * contentWidth / 11), height - 10, Math.round((x * deltaX) + minX)).attr(txt);
     }
-    // console.log(chromosome.data_points.length);
     $.each(chromosome.data_points, function(i, item){
     	var pointX = item.end + item.start / 2;
     	var x = leftgutter +((pointX - minX) / (maxX - minX) * contentWidth);
