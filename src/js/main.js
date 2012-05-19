e370245b25415d6526e3b3a4b047e8667fc3d33b
paul <paul@technotomy.com>
2012-05-19 18:49:35 +0100

    error correction

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ $(function() {
 });
 
 function plotGraph(chromosome){
-	console.log(chromosome.data_points);
+	// console.log(chromosome.data_points);
 	var axisx = [],
         axisy = [],
         maxY = 0, minY = 0,
@@ -30,32 +30,29 @@ function plotGraph(chromosome){
 		if(x < minX) minX = x;
     });
     // Draw
-    var width = 2300,
+    var width = 2100,
         height = 360,
         leftgutter = 100,
         bottomgutter = 30,
-        topgutter = 30;
+        topgutter = 30,
+        contentWidth = width - leftgutter,
+        contentHeight = height - topgutter - bottomgutter;
         r = Raphael("chart", width, height),
         txt = {"font": 'Nokia Pure Text, Arial', stroke: "none", fill: "#fff"},
         color = $("#chart").css("color"),
         deltaX = (maxX - minX) / 10,
         deltaY = (maxY - minY) / 20;
     for (var y = 0; y <= 20; y++ ) {
-        r.text(25, topgutter + (y * (height - bottomgutter - topgutter) / 21), roundNumber(((y * deltaY) - maxY), 4 ) ).attr(txt);
+        r.text(25, topgutter + (y * (contentHeight) / 21), roundNumber(((y * deltaY) - maxY), 4 ) ).attr(txt);
     }
     for (var x = 0; x <= 10; x++ ) {
-        r.text( leftgutter + (x * width / 11), height - 10, Math.round((x * deltaX) + minX)).attr(txt);
+        r.text( leftgutter + (x * contentWidth / 11), height - 10, Math.round((x * deltaX) + minX)).attr(txt);
     }
-    console.log(chromosome.data_points.length);
+    // console.log(chromosome.data_points.length);
     $.each(chromosome.data_points, function(i, item){
-    	console.log(item);
-    	console.log(' - - - - ');
     	var pointX = item.end + item.start / 2;
-    	var x = leftgutter +((pointX - minX) / (maxX - minX) * width);
-    	console.log((maxY - minY));
-    	console.log((maxY - item.y));
-    	var y = topgutter + (maxY - item.y) / (maxY - minY) * height;
-    	console.log(y);
+    	var x = leftgutter +((pointX - minX) / (maxX - minX) * contentWidth);
+    	var y = topgutter + (maxY - item.y) / (maxY - minY) * contentHeight;
     	r.circle(x, y, 2).attr({stroke: "none", fill: "#000", opacity: 1});;
     });
 
