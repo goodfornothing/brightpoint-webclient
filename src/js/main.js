02ae9e90296d2ea4ab4f1dba32f004e1d9492921
paul <paul@technotomy.com>
2012-05-19 13:53:29 +0100

    building graph with raphael

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,70 @@
-require(["jquery", "html5shiv", "models/chromosome"], function($) {
+require(["vendor/jquery", "vendor/html5shiv", "vendor/html5shiv", "models/chromosome"], function($) {
     $(function() {
-    	var chromosome = new ChromosomeSection();
-    	var height = 400, width = 800, graph = $('#chromosomeGraph .data');
+    	
 
-    	graph.css({
-    		height: height,
-    		width: width
-    	});
-    	console.log(graph);
-
-    	var i, items = chromosome.getData().items, 
-    		start = chromosome.getData().section.x1,
-    		end = chromosome.getData().section.x2, domItems = [];
-    	for(i in chromosome.getData().items){
-    		var item = items[i];
-    		var point = $('div', {"class":"point"});
-    		var css = {
-    			left : item.start - start,
-	    		width : item.end - start,
-	    		top : (1 - item.y) * height,
-	    		height : 5,
-	    		position : 'relative'
-	    	};
-    		point.css(css);
-    		domItems.push(point);
-    	}
-    	console.log(domItems);
-    	$('#chromosomeGraph .data').append(domItems);
+	    // var chromosomeSection = new ChromosomeSection(), chromosome = chromosomeSection.getData();
+	  //   var data = [],
+	  //       axisx = [],
+	  //       axisy = [];
+	  //   $.each(chromosome.items, function(i, item){
+			// data.push(parseFloat($(this).text(), 10));
+			// axisy.push($(this).text());
+			// axisx.push($(this).text());
+	  //   });
+	  //   // Draw
+	  //   var width = 800,
+	  //       height = 300,
+	  //       leftgutter = 30,
+	  //       bottomgutter = 20,
+	  //       r = Raphael("chart", width, height),
+	  //       txt = {"font": '10px "Nokia Pure Text", Arial', stroke: "none", fill: "#fff"},
+	  //       X = (width - leftgutter) / axisx.length,
+	  //       Y = (height - bottomgutter) / axisy.length,
+	  //       color = $("#chart").css("color");
+	  //       max = Math.round(X / 2) - 1;
+	  //   // r.rect(0, 0, width, height, 5).attr({fill: "#000", stroke: "none"});
+	  //   for (var i = 0, ii = axisx.length; i < ii; i++) {
+	  //       r.text(leftgutter + X * (i + .5), 294, axisx[i]).attr(txt);
+	  //   }
+	  //   for (var i = 0, ii = axisy.length; i < ii; i++) {
+	  //       r.text(10, Y * (i + .5), axisy[i]).attr(txt);
+	  //   }
+	  //   var o = 0;
+	  //   for (var i = 0, ii = axisy.length; i < ii; i++) {
+	  //       for (var j = 0, jj = axisx.length; j < jj; j++) {
+	  //           var R = data[o] && Math.min(Math.round(Math.sqrt(data[o] / Math.PI) * 4), max);
+	  //           if (R) {
+	  //               (function (dx, dy, R, value) {
+	  //                   var color = "hsb(" + [(1 - R / max) * .5, 1, .75] + ")";
+	  //                   var dt = r.circle(dx + 60 + R, dy + 10, R).attr({stroke: "none", fill: color});
+	  //                   if (R < 6) {
+	  //                       var bg = r.circle(dx + 60 + R, dy + 10, 6).attr({stroke: "none", fill: "#000", opacity: .4}).hide();
+	  //                   }
+	  //                   var lbl = r.text(dx + 60 + R, dy + 10, data[o])
+	  //                           .attr({"font": '10px Fontin-Sans, Arial', stroke: "none", fill: "#fff"}).hide();
+	  //                   var dot = r.circle(dx + 60 + R, dy + 10, max).attr({stroke: "none", fill: "#000", opacity: 0});
+	  //                   dot[0].onmouseover = function () {
+	  //                       if (bg) {
+	  //                           bg.show();
+	  //                       } else {
+	  //                           var clr = Raphael.rgb2hsb(color);
+	  //                           clr.b = .5;
+	  //                           dt.attr("fill", Raphael.hsb2rgb(clr).hex);
+	  //                       }
+	  //                       lbl.show();
+	  //                   };
+	  //                   dot[0].onmouseout = function () {
+	  //                       if (bg) {
+	  //                           bg.hide();
+	  //                       } else {
+	  //                           dt.attr("fill", color);
+	  //                       }
+	  //                       lbl.hide();
+	  //                   };
+	  //               })(leftgutter + X * (j + .5) - 60 - R, Y * (i + .5) - 10, R, data[o]);
+	  //           }
+	  //           o++;
+	  //       }
+	  //   }
     });
 });
